184e46571d54b21e465c532fee4af2d626b619b0
HSZemi <hszemi@users.noreply.github.com>
2020-08-27 00:11:38 +0200

    Configure Masters of RMS

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$start = new DateTime('2018-10-27T14:00:00.0Z');
-$end = new DateTime('2018-10-28T14:00:00.1Z');
-$stage = 'Test Run';
+$start = new DateTime('2020-08-26T00:00:00.0Z');
+$end = new DateTime('2020-09-15T23:59:59.1Z');
+$stage = 'Masters of RMS';
 $published = false;
